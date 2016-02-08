ba040fbb8d21067f019b881e8dfb7e12daf9bc79
Rofikul Islam Shahin <rshahin@Rofikuls-MacBook-Pro.local>
2016-02-08 08:46:59 +0000

    css update

--- js/main.js
+++ js/main.js
@@ -72,6 +72,22 @@ $(window).load(function() {
 });
 
 
+
+$('.select').on('click','.placeholder',function(){
+  var parent = $(this).closest('.select');
+  if ( ! parent.hasClass('is-open')){
+    parent.addClass('is-open');
+    $('.select.is-open').not(parent).removeClass('is-open');
+  }else{
+    parent.removeClass('is-open');
+  }
+}).on('click','ul>li',function(){
+  var parent = $(this).closest('.select');
+  parent.removeClass('is-open').find('.placeholder').text( $(this).text() );
+});
+
+
+
 var resizeElements;
 
 $(document).ready(function() {
